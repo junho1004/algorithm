0c27aac9cc37f983574a5196076e49bbe486a5f1
junho1004 <dlwnsgh3619@naver.com>
2023-06-22 01:32:34 +0900

    0622

--- baekjoon/DP/11053_가장 긴 증가하는 부분 수열.cpp
+++ baekjoon/DP/11053_가장 긴 증가하는 부분 수열.cpp
@@ -1,24 +1,40 @@
 #include <iostream>
+#include <algorithm>
+#define MAX_N 1010
 using namespace std;
 
-int N;
-int arr[1010];
+int N, ans;
+int arr[MAX_N];
+int dp[MAX_N];
 
 void input() {
 	cin >> N;
 	for (int i = 0; i < N; i++) {
 		cin >> arr[i];
+		dp[i] = 1;
 	}
+	ans = -1;
 }
 
 void solve() {
-
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			if (arr[i] > arr[j])
+			{
+				dp[i] = max(dp[i], dp[j] + 1);
+			}
+		}
+		ans = max(ans, dp[i]);
+	}
 }
 
-int main() 
+int main()
 {
 	freopen_s(new FILE*, "input.txt", "r", stdin);
 	input();
 	solve();
+	cout << ans;
 	return 0;
 }
