aef9d6983cc7f7858c79df039634c562834ccf99
junho1004 <dlwnsgh3619@naver.com>
2023-08-10 18:52:22 +0900

    0810

--- softeer/HSAT/2회_Garage game.cpp
+++ softeer/HSAT/2회_Garage game.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<queue>
 #include<algorithm>
-#include<string>
+#include<cstring>
 using namespace std;
 
 int ditY[] = { -1,1,0,0 };
@@ -22,84 +22,6 @@ void input() {
 	ans = 0;
 }
 
-bool isOut(int y, int x) {
-	return y < 0 || x < 0 || y >= N || x >= N;
-}
-
-int floodfill(Coord tar, int tmp[15][15]) {
-	bool used[15][15] = { 0, };
-	queue<Coord> q;
-	q.push(tar);
-	used[tar.y][tar.x] = 1;
-	int num = tmp[tar.y][tar.x];
-	tmp[tar.y][tar.x] = 0; // 터친곳 0으로
-
-	// 자동차 개수와, 직사각형 면적 계산을 위함.
-	int carCnt = 1;
-	Coord Maxi = { -1,1 };
-	Coord Mini = { 20,20 };
-
-	while (!q.empty())
-	{
-		Coord now = q.front();
-		q.pop();
-
-		Maxi.y = max(Maxi.y, now.y);
-		Maxi.x = max(Maxi.x, now.x);
-
-		Mini.y = min(Mini.y, now.y);
-		Mini.x = min(Mini.x, now.x);
-
-		for (int i = 0; i < 4; i++) {
-			int ny = now.y + ditY[i];
-			int nx = now.x + ditX[i];
-			if (isOut(ny, nx)) continue;
-			if (used[ny][nx]) continue;
-			if (tmp[ny][nx] != num) continue;
-			used[ny][nx] = 1;
-			tmp[ny][nx] = 0; // 터진 곳은 0으로 표현
-			carCnt++;
-
-			q.push({ ny,nx });
-		}
-	}
-
-	int ret = carCnt + ((Maxi.y - Mini.y) + 1) * ((Maxi.x - Mini.x) + 1);
-
-	return ret;
-}
-
-void copy_the_map(int tmp[15][15]) {
-	for (int y = 0; y < N; y++) {
-		for (int x = 0; x < N; x++) {
-			tmp[y][x] = map[y][x];
-		}
-	}
-}
-
-void drop(vector<vector<int>>& cpyV, int cpyMap[15][15])
-{
-	for (int x = 0; x < N; x++) {
-		queue<int> dropQ;
-		int spare = 0;
-
-		for (int y = N - 1; y >= 0; y--) {
-			if (cpyMap[y][x] != 0) dropQ.push(cpyMap[y][x]);
-			else spare++;
-		}
-		// 위에 차 가져오기
-		while (spare--) {
-			dropQ.push(cpyV[x].back());
-			cpyV[x].pop_back();
-		}
-
-		for (int y = N - 1; y >= 0; y--) {
-			cpyMap[y][x] = dropQ.front();
-			dropQ.pop();
-		}
-
-	}
-}
 
 
 
@@ -116,7 +38,67 @@ void dfs(int lev, int sum)
 			int now_car = tmp[y][x];
 			//빈 경우, 이미 검사한 곳
 			if (now_car == 0 || visited[y - 2 * N][x]) continue;
+			memcpy(map, tmp, sizeof(map));
+			int maxY = y;
+			int minY = y;
+			int maxX = x;
+			int minX = x;
 
+			queue<Coord> q;
+			int same = 1;
+			q.push({ y,x });
+			visited[y - 2 * N][x] = true;
+
+			while (!q.empty()) {
+				Coord now = q.front();
+				q.pop();
+				map[now.y][now.x] = 0; // 빈 처리
+
+				maxY = max(maxY, now.y);
+				minY = min(minY, now.y);
+				maxX = max(maxX, now.x);
+				minX = min(minX, now.x);
+				for (int i = 0; i < 4; i++) {
+					int ny = now.y + ditY[i];
+					int nx = now.x + ditX[i];
+					if (nx >= 0 && ny >= 2 * N && nx < N && ny < 3 * N && !visited[ny - 2 * N][nx] && tmp[ny][nx] == now_car) {
+						visited[ny - 2 * N][nx] = true;
+						q.push({ ny,nx });
+						same++;
+					}
+				}
+			}
+			// 중력
+			if (lev < 2)
+			{
+				for (int k = minX;k <= maxX;k++) {
+					for (int m = maxY;m >= minY;m--) {
+						if (map[m][k] != 0) continue;
+						int jump = 0;
+						//jump칸씩 내림
+						for (int l = m - 1;l >= 0;l--) {
+							if (map[l][k]) {
+								jump = m - l;
+								break;
+							}
+						}
+						if (jump) {
+							for (int l = m;l >= jump;l--) {
+								map[l][k] = map[l - jump][k];
+								map[l - jump][k] = 0;
+							}
+						}
+					}
+				}
+
+				dfs(lev + 1, sum + same + (maxX - minX + 1) * (maxY - minY + 1));
+			}
+
+			else
+			{
+				ans = max(ans, sum + same + (maxX - minX + 1) * (maxY - minY + 1));
+				continue;
+			}
 
 		}
 	}
@@ -129,7 +111,6 @@ int main(int argc, char** argv)
 	ios::sync_with_stdio(0);
 	input();
 	dfs(0, 0);
-	//solve();
 	cout << ans;
 
 	return 0;
