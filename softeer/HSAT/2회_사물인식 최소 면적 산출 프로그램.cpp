c84749b0b52d62079935eb67b5507e206071954a
junho1004 <dlwnsgh3619@naver.com>
2023-08-11 14:36:51 +0900

    0811

--- softeer/HSAT/2회_사물인식 최소 면적 산출 프로그램.cpp
+++ softeer/HSAT/2회_사물인식 최소 면적 산출 프로그램.cpp
@@ -1,103 +1,49 @@
-#include <iostream>
-#include <set>
-#include <vector>
-#include <queue>
-#include <map>
-#include <string>
-#include <algorithm>
-
+#include<iostream>
+#include<vector>
 using namespace std;
 
-int N;
-int K;
-int ans;
+int N, K, ans;
+vector<pair<int, int>> v[21];
 
-struct Coord {
-	int x;
-	int y;
-	int k;
-};
-
-struct Rect {
-	int l;
-	int r;
-	int t;
-	int b;
-};
-
-
-int min(int a, int b) {
-	return a > b ? b : a;
-}
-int max(int a, int b) {
-	return a > b ? a : b;
-}
-
-int area(int x1, int y1, int x2, int y2) {
-	if (x1 > x2) {
-		int t = x1;
-		x1 = x2;
-		x2 = t;
+void input() {
+	cin >> N >> K;
+	int a, b, c;
+	for (int i = 0; i < N; i++) {
+		cin >> a >> b >> c;
+		v[c - 1].push_back({ b,a });
 	}
-	if (y1 > y2) {
-		int t = y1;
-		y1 = y2;
-		y2 = t;
+	ans = 1e8;
+}
+
+void dfs(int lev, int maxY, int minY, int maxX, int minX) {
+	if (lev == K) {
+		int square = (maxY - minY) * (maxX - minX);
+		ans = min(ans, square);
+		return;
 	}
 
-	return (x2 - x1) * (y2 - y1);
-}
+	for (int i = 0; i < v[lev].size(); i++) {
+		int ny = v[lev][i].first;
+		int nx = v[lev][i].second;
+		int maxY2 = maxY; int minY2 = minY;
+		int maxX2 = maxX; int minX2 = minX;
+		maxY2 = max(maxY2, ny);
+		minY2 = min(minY2, ny);
+		maxX2 = max(maxX2, nx);
+		minX2 = min(minX2, nx);
+		int square = (maxY2 - minY2) * (maxX2 - minX2);
+		if (ans <= square) continue;
+		dfs(lev + 1, maxY2, minY2, maxX2, minX2);
 
-int area(Rect r) {
-	return area(r.l, r.t, r.r, r.b);
-}
-
-int f(vector<vector<pair<int, int>>>& colors, int cur, Rect rect) {
-	if (cur > K) return area(rect);
-
-	for (int i = 0; i < colors[cur].size(); ++i) {
-		Rect r = rect;
-		if (cur == 1) {
-			r.l = colors[cur][i].first;
-			r.r = colors[cur][i].first;
-			r.t = colors[cur][i].second;
-			r.b = colors[cur][i].second;
-		}
-		else {
-			r.l = min(r.l, colors[cur][i].first);
-			r.r = max(r.r, colors[cur][i].first);
-			r.t = min(r.t, colors[cur][i].second);
-			r.b = max(r.b, colors[cur][i].second);
-
-			if (ans <= area(r)) continue;
-		}
-
-		ans = min(ans, f(colors, cur + 1, r));
 	}
-
-	return ans;
 }
-
 
 int main() {
-	cin >> N;
-	cin >> K;
-
-	vector<Coord> points(N);
-	vector<vector<pair<int, int>>> colors(K + 1);
-	for (int i = 0; i < N; ++i) {
-		int x, y, k;
-		cin >> x >> y >> k;
-
-		points[i].x = x;
-		points[i].y = y;
-		points[i].k = k;
-		colors[k].push_back({ x, y });
-	}
-
-	ans = 987654321;
-	f(colors, 1, Rect());
-
+	freopen_s(new FILE*, "input.txt", "r", stdin);
+	cin.tie(0);cout.tie(0);
+	ios::sync_with_stdio(0);
+	input();
+	dfs(0, -1e8, 1e8, -1e8, 1e8);
 	cout << ans;
 	return 0;
 }
