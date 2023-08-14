5392f5cfeb6eb69f17c62228560626ad74464adb
junho1004 <dlwnsgh3619@naver.com>
2023-08-14 14:47:16 +0900

    0814

--- baekjoon/삼성 SW 역량 테스트 기출 문제/사다리 조작.cpp
+++ baekjoon/삼성 SW 역량 테스트 기출 문제/사다리 조작.cpp
@@ -1,20 +1,24 @@
 #include<iostream>
 using namespace std;
-// 10*30 * 10 * 30 * 10 * 30
-// 27,000,000
+// 조합을 이용해서 중복 없이 사다리를 놓을 2차원 좌표를 뽑는다.
+// (10 * 30) C0 + (10 * 30) C1 + (10 * 30) C2 + (10 * 30) C3
+// 1 + 300 + 44,850 + 4,455,100
+// 사다리 안 놓기
 // 사다리 하나 놓기
 // 사다리 두개 놓기
 // 사다리 세개 놓기
 // 2초 512MB
-// 배열이 낫다 사다리의 위치만 기록 1,1 은 1~2연결
+// 배열이 낫다 사다리의 위치만 기록
+// 1,1 은 1~2연결
 // 3,4는 4~5연결
 
 int H, W, cnt_ladder;
-bool map[31][11];
+bool map[32][12];
 bool ansFlag;
+int now_ladder_cnt;
 
 void input() {
-	cin >> W >> H >> cnt_ladder;
+	cin >> W >> cnt_ladder >> H;
 	int a, b;
 	for (int i = 0; i < cnt_ladder; i++) {
 		cin >> a >> b;
@@ -24,52 +28,64 @@ void input() {
 }
 // 세로선 5개 가로선 6개면
 // 6,4가 최대 주어질 수 있다
-bool play() 
+bool play()
 {
-	for (int sero = 1; sero <= W; sero++) 
+	for (int line = 1; line <= W; line++)
 	{
-		int now = sero;
+		int flag = 0;
+		int sero = line;
+		int garo = 0;
 
-		
-		for (int garo = 1; garo <= H; garo++) 
-		{
-			map[now][garo] == 0;
+		for (int garo = 0; garo < H; garo++) {
+			// 오른쪽
+			if (map[garo + 1][sero]) sero++;
+			// 왼쪽
+			else if (map[garo + 1][sero - 1]) sero--;
+
 		}
+
+		// 시작라인과 끝라인이 같지 않으면 즉시 종료.
+		if (line != sero) return 0;
 	}
+	// 모두 다 통과하면 합격
+	return 1;
 }
-// 1,1가다가 1,1사다리면 1,2로
-// 1,2 -> 2,2  3,2사다리면 3,3으로
-// 3,3 -> 4,3 4,2사다리면 4,2로
-void dfs(int lev, int cnt) {
+
+
+void dfs(int lev, int startY, int startX) {
 	if (ansFlag) return;
 
-	if (lev == cnt) 
+	if (now_ladder_cnt == lev)
 	{
 		// 사다리타고 내려가
 		if (play()) ansFlag = 1;
 		return;
 	}
 
-	for (int y = 1; y <= H; y++) {
-		for (int x = 1; x < W; x++) {
-			if (map[y][x]) continue; // 사다리이미 있다면 설치 불가
+	// 조합(used + start)
+	for (int y = startY; y <= H; y++) {
+		for (int x = startX; x < W; x++) {
+			// 사다리가 이미 있다면(자리 양옆 포함) 설치 불가
+			if (map[y][x] || map[y][x - 1] || map[y][x + 1]) continue;
 			map[y][x] = true;
-			dfs(lev + 1, cnt);
+			dfs(lev + 1, y, x);
 			map[y][x] = false;
 		}
+		startX = 0; // 중요!!!
 	}
 }
 
 void solve() {
-	for (int i = 0; i <= 3; i++) 
+	for (int i = 0; i <= 3; i++)
 	{
+		now_ladder_cnt = i;
 		// 사다리 개수가 i 개 일때 가능?
-		dfs(0, i);
+		dfs(0, 1, 1);
 
-		if (ansFlag) 
+		if (ansFlag)
 		{
-			cout << i;
-			break;
+			cout << now_ladder_cnt;
+			return;
 		}
 	}
 	cout << -1;
@@ -79,6 +95,6 @@ int main() {
 	freopen_s(new FILE*, "input.txt", "r", stdin);
 	input();
 	solve();
-	
+
 	return 0;
 }
