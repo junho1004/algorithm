ebcc9e3b6d64bcb580395d23c9f8787b948241b8
junho1004 <dlwnsgh3619@naver.com>
2023-11-20 16:01:02 +0900

    231120

--- baekjoon/삼성 SW 역량 테스트 기출 문제/낚시왕.cpp
+++ baekjoon/삼성 SW 역량 테스트 기출 문제/낚시왕.cpp
@@ -1,48 +1,116 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <cstring>
 using namespace std;
 
-int H, W, M;
 
 // (H * W)마리 상어 이동 * H(낚시꾼 탐색) * W(마지막 열까지) == 1억
 // 상어의 속력은 최대 1000, 1000번을 반복하는 것이 아니라
 // 적절한 연산을 통해 반복을 줄여야한다.
-// 모든 이동이 끝나고 위치가 같은 상어에 대해 연산을 해준다.
-// 맵을 사용하지 않고 벡터로만(정렬) 관리해도 될 것 같다.
+// 각 상어마다 이동이 끝날 때 카피맵에 이미 상어가 있다면 크기 비교를 해줘야한다.
+// 속력 % ((H - 1 또는 W - 1) x 2)[제자리] = 현 위치에서 움직여야하는 거리
+// 맵(1부터 이용)을 두개 사용해서 복사하면서 진행.
+
+
+int H, W, M;
+int fish_cnt = 0;
 
 struct Node {
-	int y, x, speed, dir, weigh;
+	int speed, dir, weigh;
 };
-vector<Node> v;
+Node map[105][105];
+Node CopyMap[105][105];
+
+// 1,2,3,4 상 하 우 좌
+int dit[] = { 0,-1,1,1,-1 };
 
 void input() {
 	cin >> H >> W >> M;
-	int a, b, c, d, e;
+	int r, c, s, d, z;
 	for (int i = 0; i < M; i++)
 	{
-		cin >> a >> b >> c >> d >> e;
-		v.push_back({ a,b,c,d,e });
+		cin >> r >> c >> s >> d >> z;
+		map[r][c] = { s,d,z };
 	}
 }
 
-void moveShark() {
+void move_eat_Shark() {
+	for (int y = 1; y <= H; y++) {
+		for (int x = 1; x <= W; x++) {
+			if (map[y][x].weigh != 0) // 상어 있다면
+			{
+				int shark_speed = map[y][x].speed;
+				int shark_weigh = map[y][x].weigh;
+				int shark_dir = map[y][x].dir;
+				int move_dist; // 움직여야하는 거리
+				int now;
 
+				if (shark_dir == 1 || shark_dir == 2) // 상하 방향
+				{
+					now = y;
+					move_dist = shark_speed % ((H - 1) * 2);
+					while (move_dist)
+					{
+						if (now == 1) shark_dir = 2;
+						else if (now == H) shark_dir = 1;
+						now = now + dit[shark_dir]; // 상 or 하						
+						move_dist--;
+					}
+
+					if (CopyMap[now][x].weigh < shark_weigh) { // 잡아먹기 or 넘어가기
+						CopyMap[now][x] = { shark_speed, shark_dir, shark_weigh };
+					}
+				}
+
+				else // 좌우 방향 
+				{
+					now = x;
+					move_dist = shark_speed % ((W - 1) * 2);
+					while (move_dist)
+					{
+						if (now == 1) shark_dir = 3;
+						else if (now == W) shark_dir = 4;
+						now = now + dit[shark_dir]; // 우 or 좌
+						move_dist--;
+					}
+					if (CopyMap[y][now].weigh < shark_weigh) { // 잡아먹기 or 넘어가기
+						CopyMap[y][now] = { shark_speed, shark_dir, shark_weigh };
+					}
+				}
+
+			}
+		}
+	}
 }
 
-void eatShark() {
 
+void fishing(int tarCol) {
+	for (int y = 1; y <= H; y++) {
+		if (map[y][tarCol].weigh != 0) { // 상어 있다면
+			fish_cnt += map[y][tarCol].weigh; // 상어 무게 추가
+			map[y][tarCol] = { 0,0,0 }; // 상어 잡았으면 초기화
+			return; // 한마리 잡았으면 빠져나와
+		}
+	}
 }
-void fishing() {
 
+void Copy() {
+	for (int y = 1; y <= H; y++) {
+		for (int x = 1; x <= W; x++) {
+			map[y][x] = CopyMap[y][x];
+		}
+	}
 }
 
 void solve()
 {
-	for (int x = 0; x < W; x++) {
-		moveShark();
-		eatShark();
-		fishing();
+	for (int x = 1; x <= W; x++) {
+		fishing(x);
+
+		memset(CopyMap, 0, sizeof(CopyMap));
+
+		move_eat_Shark();
+
+		Copy();
 	}
 }
 
@@ -51,7 +119,9 @@ int main()
 	cin.tie(0); cout.tie(0);
 	ios::sync_with_stdio(0);
 	freopen_s(new FILE*, "input.txt", "r", stdin);
+
 	input();
 	solve();
+	cout << fish_cnt;
 	return 0;
 }
